40b9ddd5200c7a6c867a8da721de390130b06740
Juan Ramirez <rami8224.2007@gmail.com>
2022-05-16 15:27:02 -0400

    second commit

--- app.js
+++ app.js
@@ -3,7 +3,22 @@ const app = express()
 const port = 4000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Juan Ramirez')
+})
+
+//return a see other
+app.get('/seeother', (req, res) => {
+  res.status(204).send('204 no content!')
+})
+
+//return a error/status code
+app.get('/sample', (req, res) => {
+  res.status(404).send('404 this is empty!')
+})
+
+//wildcard route
+app.get('*', (req, res) => {
+  res.send('wildcard route')
 })
 
 app.listen(port, () => {
